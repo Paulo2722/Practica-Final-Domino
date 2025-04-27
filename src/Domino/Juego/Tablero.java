6fa47b59f69f3f182b61cebc698b2b0ab524bbd8
Paulo <pescanellasotero@gmail.com>
2025-04-27 22:49:50 +0200

    Creamos la estructura del tablero de juego

--- src/Domino/Juego/Tablero.java
+++ src/Domino/Juego/Tablero.java
@@ -1,4 +1,20 @@
 package Domino.Juego;
 
 public class Tablero {
+    private final int filas = 10;
+    private final int columnas = 10;
+    private Ficha[][] tablero;
+
+    public Tablero() {
+        tablero = new Ficha[filas][columnas];
+    }
+
+    public void imprimirTablero(){
+        for (int i = 0; i < filas; i++){
+            for(int j = 0; j < columnas; j++){
+                System.out.println(tablero[i][j].toString());
+            }
+            System.out.println();
+        }
+    }
 }
