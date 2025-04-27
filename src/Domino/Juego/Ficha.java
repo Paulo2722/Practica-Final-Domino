6b781ada92bbb48927b8c1d6b5039d15177c1343
Paulo <pescanellasotero@gmail.com>
2025-04-27 22:41:12 +0200

    Creamos los metodos con los que podremos representar una ficha del domino

--- src/Domino/Juego/Ficha.java
+++ src/Domino/Juego/Ficha.java
@@ -1,4 +1,32 @@
 package Domino.Juego;
 
 public class Ficha {
+    private int ladoA;
+    private int ladoB;
+
+    public Ficha(int ladoA, int ladoB){
+        this.ladoA = ladoA;
+        this.ladoB = ladoB;
+    }
+
+    public int getLadoA() {
+        return ladoA;
+    }
+
+    public int getLadoB() {
+        return ladoB;
+    }
+
+    public void setLadoA(int ladoA) {
+        this.ladoA = ladoA;
+    }
+
+    public void setLadoB(int ladoB) {
+        this.ladoB = ladoB;
+    }
+
+    @Override
+    public String toString(){
+        return "[" + ladoA + "|" + ladoB + "]";
+    }
 }
