2ec8e9722cb75bc6bb27260d270892d3a37413dc
Paulo <pescanellasotero@gmail.com>
2025-04-29 12:09:33 +0200

    Creamos una ArrayList con la que guardamos todas las fichas que creamos(28)

--- src/Domino/Juego/Tablero.java
+++ src/Domino/Juego/Tablero.java
@@ -1,12 +1,28 @@
 package Domino.Juego;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tablero {
     private final int filas = 10;
     private final int columnas = 10;
     private Ficha[][] tablero;
+    private List<Ficha> fichas;
 
     public Tablero() {
+        this.fichas = crearFichas();
         tablero = new Ficha[filas][columnas];
+    }
+
+    public List<Ficha> crearFichas(){
+        List<Ficha> fichas = new ArrayList<>();
+
+        for (int i = 0; i <= 6; i++){
+            for (int j = i; j <= 6; j++){
+                fichas.add(new Ficha(i,j));
+            }
+        }
+        return fichas;
     }
 
     public void imprimirTablero(){
