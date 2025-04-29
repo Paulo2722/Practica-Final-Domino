e9e3bfc829ce979bcc362324408e03d1b477e60c
Paulo <pescanellasotero@gmail.com>
2025-04-29 13:24:27 +0200

    Creo atributos y métodos en la clase Jugador para poder guardar el nombre de un jugador y su mano

--- src/Domino/Juego/Jugador.java
+++ src/Domino/Juego/Jugador.java
@@ -1,4 +1,34 @@
 package Domino.Juego;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Jugador {
+    private String nombre;
+    private List<Ficha> mano;
+
+    public Jugador(String nombre){
+        this.nombre = nombre;
+        this.mano = new ArrayList<>();
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public List<Ficha> getMano(){
+        return mano;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public void recibirFicha(Ficha ficha){
+        mano.add(ficha);
+    }
+
+    public void mostrarMano(){
+        System.out.println(nombre + ", tu mano: " + mano);
+    }
 }
