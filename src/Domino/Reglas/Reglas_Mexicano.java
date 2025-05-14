c2c22f36d1fca6985cbea11476bcd984933b6da9
Paulo <pescanellasotero@gmail.com>
2025-05-14 10:40:50 +0200

    Implemento las reglas de la modalidad mexicana

--- src/Domino/Reglas/Reglas_Mexicano.java
+++ src/Domino/Reglas/Reglas_Mexicano.java
@@ -1,4 +1,82 @@
 package Domino.Reglas;
 
-public class Reglas_Mexicano {
+import Domino.Juego.Ficha;
+import Domino.Juego.Jugador;
+
+import java.util.List;
+
+public class Reglas_Mexicano extends Reglas{
+
+    //!!!!!Me falta lo de la traca
+    @Override
+    public boolean rondaGanada(Jugador jugador){
+        return jugador.getMano().isEmpty();
+    }
+
+    @Override
+    public int puntuacionFinalPartida() {
+        return 200;
+    }
+
+    @Override
+    public boolean soloSeJuegaEnParejas(){
+        return false;
+    }
+
+    @Override
+    public boolean tieneTraca() {
+        return true;
+    }
+
+    public int puntuacionTraca(){
+        if (tieneTraca()){
+
+        }
+    }
+
+    @Override
+    public int jugadorQueEmpiezaLaRonda(List<Jugador> jugador) {
+        int jugadorQueEmpieza = 0;
+        int fichaMasGrande = 0;
+
+        for (int i = 0; i < jugador.size(); i++){
+            List<Ficha> mano = jugador.get(i).getMano();
+
+            for (int j = 0; j < mano.size(); j++){
+                Ficha ficha = mano.get(j);
+
+                if (ficha.getLadoA() + ficha.getLadoB() > fichaMasGrande){
+                    fichaMasGrande = ficha.getLadoA() + ficha.getLadoB();
+                    jugadorQueEmpieza = i;
+                }
+            }
+        }
+        return jugadorQueEmpieza;
+    }
+
+    @Override
+    public int puntuacionJugador(List<Jugador> jugadores) {
+        int puntuacion = 0;
+        Jugador jugador = null;
+
+        for (int i = 0; i < jugadores.size(); i++){
+            List<Ficha> mano = jugadores.get(i).getMano();
+
+            if (mano.isEmpty()){
+                jugador = jugadores.get(i);
+            }else{
+                for (int j = 0; j < mano.size(); j++){
+                    Ficha ficha = mano.get(j);
+                    puntuacion += ficha.getLadoA() + ficha.getLadoB();
+                }
+            }
+        }
+        if (jugador != null){
+            jugador.setPuntuacion(puntuacion);
+        }
+        if (puntuacion >= puntuacionFinalPartida()){
+            System.out.println("El jugador" + jugador.getNombre() + "ha ganado la partida");
+        }
+        return puntuacion;
+    }
 }
