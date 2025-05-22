816c97e858d86a98e4f6d8c1d65c3eb1ae39d37b
Paulo <pescanellasotero@gmail.com>
2025-05-22 09:11:18 +0200

    Implemento las reglas de la modalidad Colombiano

--- src/Domino/Reglas/Reglas_Colombiano.java
+++ src/Domino/Reglas/Reglas_Colombiano.java
@@ -1,4 +1,81 @@
 package Domino.Reglas;
 
-public class Reglas_Colombiano {
+import Domino.Juego.Ficha;
+import Domino.Juego.Jugador;
+import Domino.Juego.Parejas;
+
+import java.util.List;
+
+public class Reglas_Colombiano extends Reglas{
+
+    @Override
+    public boolean rondaGanada(Jugador jugador) {
+        return jugador.getMano().isEmpty();
+    }
+
+    @Override
+    public int puntuacionFinalPartida() {
+        return 100;
+    }
+
+    @Override
+    public int puntuacionJugador(List<Jugador> jugadores, List<Parejas> parejas) {
+        for (int i = 0; i < parejas.size(); i++) {
+            Parejas pareja = parejas.get(i);
+            List<Jugador> jugadoresPareja = pareja.getJugadoresPareja();
+            Jugador jugadorGanador = null;
+            int puntuacionPareja = 0;
+
+            for (int j = 0; j < jugadoresPareja.size(); j++) {
+                Jugador jugador = jugadoresPareja.get(j);
+                List<Ficha> mano = jugador.getMano();
+
+                if (mano.isEmpty()) {
+                    jugadorGanador = jugador;
+
+                } else {
+
+                    for (int n = 0; n < mano.size(); n++) {
+                        Ficha ficha = mano.get(n);
+                        puntuacionPareja += ficha.getLadoA() + ficha.getLadoB();
+                    }
+                }
+            }
+            pareja.setPuntuacion(pareja.getPuntuacionEquipo() + puntuacionPareja);
+
+            if (jugadorGanador != null && jugadorGanador.getPuntuacion() >= puntuacionFinalPartida()) {
+                System.out.println("La pareja " + pareja.getNombre() + " ha ganado la partida");
+            }
+        }
+        return 0;
+    }
+
+    //Falta implementar lo de quien empieza despues de la primera ronda
+    @Override
+    public int jugadorQueEmpiezaLaRonda(List<Jugador> jugador) {
+        int jugadorQueEmpieza = 0;
+
+        for (int i = 0; i < jugador.size(); i++) {
+            List<Ficha> mano = jugador.get(i).getMano();
+
+            for (int j = 0; j < mano.size(); j++) {
+                Ficha ficha = mano.get(j);
+
+                if (ficha.getLadoA() == 6 && ficha.getLadoB() == 6) {
+                    jugadorQueEmpieza = i;
+                    }
+                }
+            }
+        return jugadorQueEmpieza;
+    }
+
+    @Override
+    public boolean tieneTraca() {
+        return true;
+    }
+
+    @Override
+    public boolean modoDeJuegoEnParejas() {
+        return true;
+    }
 }
