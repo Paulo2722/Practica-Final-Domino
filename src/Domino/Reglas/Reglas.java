0434a5cc7f40ccb6edc0499a0640d0e385c3b136
Paulo <pescanellasotero@gmail.com>
2025-06-05 09:59:51 +0200

    Modifico y muevo el método para elegir el modo de juego a la clase padre

--- src/Domino/Reglas/Reglas.java
+++ src/Domino/Reglas/Reglas.java
@@ -4,6 +4,7 @@ import Domino.Juego.Jugador;
 import Domino.Juego.Parejas;
 
 import java.util.List;
+import java.util.Scanner;
 
 public abstract class Reglas {
 
@@ -17,7 +18,25 @@ public abstract class Reglas {
 
     public abstract int jugadorQueEmpiezaLaRonda(List<Jugador> jugador);
 
-    public abstract boolean modoDeJuegoEnParejas();
+    public boolean modoDeJuegoEnParejas() {
+        Scanner sc = new Scanner(System.in);
+        String respuesta;
+
+        while (true) {
+            System.out.println("Elige el modo de juego:");
+            System.out.println("Pulsa 'I' si quieres el modo individual, pulsa 'P' si quieres el modo en parejas");
+
+            respuesta = sc.nextLine().toLowerCase();
+
+            if (respuesta.equals("i")){
+                return false;
+            }else if (respuesta.equals("p")){
+                return true;
+            }else{
+                System.out.println("El valor introducido no es valido");
+            }
+        }
+    }
 
     public boolean hayTranca() {
         return haHabidoTranca;
