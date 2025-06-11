44822fbea511dc3dc0d99f3daddfcacc3d3a7737
Paulo <pescanellasotero@gmail.com>
2025-06-11 20:19:29 +0200

    Creo un nuevo método que usare para sumar puntuación a los jugadores de una manera en concreto en caso de que haya una tranca durante la partida

--- src/Domino/Reglas/Reglas.java
+++ src/Domino/Reglas/Reglas.java
@@ -1,5 +1,6 @@
 package Domino.Reglas;
 
+import Domino.Juego.Ficha;
 import Domino.Juego.Jugador;
 import Domino.Juego.Parejas;
 
@@ -44,5 +45,93 @@ public abstract class Reglas {
 
     public void setTranca(boolean valor) {
         this.haHabidoTranca = valor;
+    }
+
+    public void resolverTranca(List<Jugador> jugadores, List<Parejas> parejas, boolean modoParejas) {
+        if (!hayTranca()){
+            return;
+        }
+
+        if (modoParejas){
+            Parejas ganador = null;
+            int menorSuma = 9999;
+
+            for (int i = 0; i < parejas.size(); i++){
+                Parejas pareja = parejas.get(i);
+                int sumaPareja = 0;
+                List<Jugador> jugadoresPareja = pareja.getJugadoresPareja();
+
+                for (int j = 0; j < jugadoresPareja.size(); j++){
+                    List<Ficha> mano = jugadoresPareja.get(j).getMano();
+
+                    for (int k = 0; k < mano.size(); k++){
+                        Ficha ficha = mano.get(k);
+                        sumaPareja += ficha.getLadoA() + ficha.getLadoB();
+                    }
+                }
+                if (sumaPareja < menorSuma) {
+                    menorSuma = sumaPareja;
+                    ganador = pareja;
+                }
+            }
+
+            if (ganador != null) {
+                int puntosOtrosJugadores = 0;
+
+                for (int i = 0; i < parejas.size(); i++){
+                    if (parejas.get(i) != ganador) {
+                        List<Jugador> jugadoresPareja = parejas.get(i).getJugadoresPareja();
+
+                        for (int j = 0; j < jugadoresPareja.size(); j++){
+                            List<Ficha> mano = jugadoresPareja.get(j).getMano();
+
+                            for (int k = 0; k < mano.size(); k++) {
+                                Ficha ficha = mano.get(k);
+                                puntosOtrosJugadores += ficha.getLadoA() + ficha.getLadoB();
+                            }
+                        }
+                    }
+                }
+
+                ganador.setPuntuacion(puntosOtrosJugadores);
+            }
+
+        } else {
+            Jugador ganador = null;
+            int menorSuma = 9999;
+
+            for (int i = 0; i < jugadores.size(); i++) {
+                Jugador jugador = jugadores.get(i);
+                int sumaJugador = 0;
+                List<Ficha> mano = jugador.getMano();
+
+                for (int j = 0; j < mano.size(); j++) {
+                    Ficha ficha = mano.get(j);
+                    sumaJugador += ficha.getLadoA() + ficha.getLadoB();
+                }
+
+                if (sumaJugador < menorSuma) {
+                    menorSuma = sumaJugador;
+                    ganador = jugador;
+                }
+            }
+
+            if (ganador != null) {
+                int puntosOtrosJugadores = 0;
+
+                for (int i = 0; i < jugadores.size(); i++) {
+                    if (jugadores.get(i) != ganador) {
+                        List<Ficha> mano = jugadores.get(i).getMano();
+
+                        for (int j = 0; j < mano.size(); j++) {
+                            Ficha ficha = mano.get(j);
+                            puntosOtrosJugadores += ficha.getLadoA() + ficha.getLadoB();
+                        }
+                    }
+                }
+
+                ganador.setPuntuacion(puntosOtrosJugadores);
+            }
+        }
     }
 }
